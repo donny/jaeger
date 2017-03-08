9842c1bd9f013448770a04d65f769165b7978db3
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-03-08 17:08:56 +1100

    Use enum to represent result

--- Sources/SwiftBuildkite/SwiftBuildkite.swift
+++ Sources/SwiftBuildkite/SwiftBuildkite.swift
@@ -1,6 +1,11 @@
 import KituraRequest
 import SwiftyJSON
 
+public enum Result<Value> {
+    case success(Value)
+    case failure(Error)
+}
+
 public class SwiftBuildkite {
     private let token: String
     private let endpoint = "https://api.buildkite.com/v2"
@@ -9,115 +14,173 @@ public class SwiftBuildkite {
         self.token = token
     }
     
-    private func request(_ method: Request.Method, _ URL: String, parameters: Request.Parameters? = nil, callback: @escaping (JSON) -> Void) -> Void {
+    private func request(_ method: Request.Method, _ URL: String, parameters: Request.Parameters? = nil,
+                         callback: @escaping (Result<JSON>) -> Void) -> Void {
         KituraRequest.request(method, endpoint + URL,
                               parameters: parameters,
                               encoding: JSONEncoding.default,
                               headers: ["Authorization": "Bearer \(token)"]
         ).response { request, response, data, error in
-            guard let data = data else { return }
-            callback(JSON(data: data))
+            if let error = error {
+                callback(Result.failure(error))
+                return
+            }
+            
+            guard let data = data else { return /* callback() */ }
+            callback(Result.success(JSON(data: data)))
         }
     }
     
     // Organizations
     
-    public func organizations(callback: @escaping (Organizations) -> Void) -> Void {
-        request(.get, "/organizations") { json in
-            let orgs = Organizations(json: json)
-            callback(orgs)
+    public func organizations(callback: @escaping (Result<Organizations>) -> Void) -> Void {
+        request(.get, "/organizations") { result in
+            switch result {
+            case .success(let value):
+                callback(Result.success(Organizations(json: value)))
+            case .failure(let error):
+                callback(Result.failure(error))
+            }
         }
     }
     
-    public func organization(_ orgSlug: String, callback: @escaping (Organization) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)") { json in
-            let org = Organization(json: json)
-            callback(org)
+    public func organization(_ orgSlug: String, callback: @escaping (Result<Organization>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)") { result in
+            switch result {
+            case .success(let value):
+                callback(Result.success(Organization(json: value)))
+            case .failure(let error):
+                callback(Result.failure(error))
+            }
         }
     }
 
     // Pipelines
     
-    public func pipelines(_ orgSlug: String, callback: @escaping (Pipelines) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/pipelines") { json in
-            let pipelines = Pipelines(json: json)
-            callback(pipelines)
+    public func pipelines(_ orgSlug: String, callback: @escaping (Result<Pipelines>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/pipelines") { result in
+            switch result {
+            case .success(let value):
+                callback(Result.success(Pipelines(json: value)))
+            case .failure(let error):
+                callback(Result.failure(error))
+            }
         }
     }
 
-    public func pipeline(_ orgSlug: String, _ slug: String, callback: @escaping (Pipeline) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/pipelines/\(slug)") { json in
-            let pipeline = Pipeline(json: json)
-            callback(pipeline)
+    public func pipeline(_ orgSlug: String, _ slug: String, callback: @escaping (Result<Pipeline>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/pipelines/\(slug)") { result in
+            switch result {
+            case .success(let value):
+                callback(Result.success(Pipeline(json: value)))
+            case .failure(let error):
+                callback(Result.failure(error))
+            }
         }
     }
     
     // Builds
 
-    public func builds(callback: @escaping (Builds) -> Void) -> Void {
-        request(.get, "/builds") { json in
-            let builds = Builds(json: json)
-            callback(builds)
+    public func builds(callback: @escaping (Result<Builds>) -> Void) -> Void {
+        request(.get, "/builds") { result in
+            switch result {
+            case .success(let value):
+                callback(Result.success(Builds(json: value)))
+            case .failure(let error):
+                callback(Result.failure(error))
+            }
         }
     }
     
-    public func builds(_ orgSlug: String, callback: @escaping (Builds) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/builds") { json in
-            let builds = Builds(json: json)
-            callback(builds)
+    public func builds(_ orgSlug: String, callback: @escaping (Result<Builds>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/builds") { result in
+            switch result {
+            case .success(let value):
+                callback(Result.success(Builds(json: value)))
+            case .failure(let error):
+                callback(Result.failure(error))
+            }
         }
     }
     
-    public func builds(_ orgSlug: String, _ pipeSlug: String, callback: @escaping (Builds) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds") { json in
-            let builds = Builds(json: json)
-            callback(builds)
+    public func builds(_ orgSlug: String, _ pipeSlug: String, callback: @escaping (Result<Builds>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds") { result in
+            switch result {
+            case .success(let value):
+                callback(Result.success(Builds(json: value)))
+            case .failure(let error):
+                callback(Result.failure(error))
+            }
         }
     }
 
-    public func build(_ orgSlug: String, _ pipeSlug: String, _ number: Int, callback: @escaping (Build) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds/\(number)") { json in
-            let build = Build(json: json)
-            callback(build)
+    public func build(_ orgSlug: String, _ pipeSlug: String, _ number: Int, callback: @escaping (Result<Build>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds/\(number)") { result in
+            switch result {
+            case .success(let value):
+                callback(Result.success(Build(json: value)))
+            case .failure(let error):
+                callback(Result.failure(error))
+            }
         }
     }
     
     // Agents
     
-    public func agents(_ orgSlug: String, callback: @escaping (Agents) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/agents") { json in
-            let agents = Agents(json: json)
-            callback(agents)
+    public func agents(_ orgSlug: String, callback: @escaping (Result<Agents>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/agents") { result in
+            switch result {
+            case .success(let value):
+                callback(Result.success(Agents(json: value)))
+            case .failure(let error):
+                callback(Result.failure(error))
+            }
         }
     }
     
-    public func agent(_ orgSlug: String, _ id: String, callback: @escaping (Agent) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/agents/\(id)") { json in
-            let agent = Agent(json: json)
-            callback(agent)
+    public func agent(_ orgSlug: String, _ id: String, callback: @escaping (Result<Agent>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/agents/\(id)") { result in
+            switch result {
+            case .success(let value):
+                callback(Result.success(Agent(json: value)))
+            case .failure(let error):
+                callback(Result.failure(error))
+            }
         }
     }
     
     // Artifacts
 
-    public func artifacts(_ orgSlug: String, _ pipeSlug: String, _ number: Int, callback: @escaping (Artifacts) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds/\(number)/artifacts") { json in
-            let artifacts = Artifacts(json: json)
-            callback(artifacts)
+    public func artifacts(_ orgSlug: String, _ pipeSlug: String, _ number: Int, callback: @escaping (Result<Artifacts>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds/\(number)/artifacts") { result in
+            switch result {
+            case .success(let value):
+                callback(Result.success(Artifacts(json: value)))
+            case .failure(let error):
+                callback(Result.failure(error))
+            }
         }
     }
     
-    public func artifacts(_ orgSlug: String, _ pipeSlug: String, _ number: Int, _ id: String, callback: @escaping (Artifacts) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds/\(number)/jobs/\(id)/artifacts") { json in
-            let artifacts = Artifacts(json: json)
-            callback(artifacts)
+    public func artifacts(_ orgSlug: String, _ pipeSlug: String, _ number: Int, _ id: String, callback: @escaping (Result<Artifacts>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds/\(number)/jobs/\(id)/artifacts") { result in
+            switch result {
+            case .success(let value):
+                callback(Result.success(Artifacts(json: value)))
+            case .failure(let error):
+                callback(Result.failure(error))
+            }
         }
     }
 
-    public func artifact(_ orgSlug: String, _ pipeSlug: String, _ number: Int, _ id: String, callback: @escaping (Artifact) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds/\(number)/artifacts/\(id)") { json in
-            let artifact = Artifact(json: json)
-            callback(artifact)
+    public func artifact(_ orgSlug: String, _ pipeSlug: String, _ number: Int, _ id: String, callback: @escaping (Result<Artifact>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds/\(number)/artifacts/\(id)") { result in
+            switch result {
+            case .success(let value):
+                callback(Result.success(Artifact(json: value)))
+            case .failure(let error):
+                callback(Result.failure(error))
+            }
         }
     }
 
