3b6cc84a10c16ca414c0a51b78f0dd9b0bc98da7
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-03-10 06:05:01 +1100

    Remove client-side code

--- Package.swift
+++ Package.swift
@@ -3,8 +3,7 @@ import PackageDescription
 let package = Package(
     name: "jaeger",
     targets: [
-        Target(name: "SwiftBuildkite"),
-        Target(name: "Jaeger", dependencies: [.Target(name: "SwiftBuildkite")])
+        Target(name: "SwiftBuildkite")
     ],
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0, minor: 7),
