9589af1b82002d2a1b137c88e77f3662968fad88
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-03-09 16:20:22 +1100

    Add request parameters

--- Sources/SwiftBuildkite/SwiftBuildkite.swift
+++ Sources/SwiftBuildkite/SwiftBuildkite.swift
@@ -24,7 +24,6 @@ public class SwiftBuildkite {
                          callback: @escaping (Result<JSON>) -> Void) -> Void {
         KituraRequest.request(method, endpoint + URL,
                               parameters: parameters,
-                              encoding: JSONEncoding.default,
                               headers: ["Authorization": "Bearer \(token)"]
         ).response { request, response, data, error in
             if let error = error {
@@ -55,8 +54,8 @@ public class SwiftBuildkite {
     
     // Organizations
     
-    public func organizations(callback: @escaping (Result<Organizations>) -> Void) -> Void {
-        request(.get, "/organizations") { result in
+    public func organizations(parameters: [String: Any]? = nil, callback: @escaping (Result<Organizations>) -> Void) -> Void {
+        request(.get, "/organizations", parameters: parameters) { result in
             switch result {
             case .success(let value):
                 callback(Result.success(Organizations(json: value)))
@@ -66,8 +65,8 @@ public class SwiftBuildkite {
         }
     }
     
-    public func organization(_ orgSlug: String, callback: @escaping (Result<Organization>) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)") { result in
+    public func organization(_ orgSlug: String, parameters: [String: Any]? = nil, callback: @escaping (Result<Organization>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)", parameters: parameters) { result in
             switch result {
             case .success(let value):
                 callback(Result.success(Organization(json: value)))
@@ -79,8 +78,8 @@ public class SwiftBuildkite {
 
     // Pipelines
     
-    public func pipelines(_ orgSlug: String, callback: @escaping (Result<Pipelines>) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/pipelines") { result in
+    public func pipelines(_ orgSlug: String, parameters: [String: Any]? = nil, callback: @escaping (Result<Pipelines>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/pipelines", parameters: parameters) { result in
             switch result {
             case .success(let value):
                 callback(Result.success(Pipelines(json: value)))
@@ -90,8 +89,8 @@ public class SwiftBuildkite {
         }
     }
 
-    public func pipeline(_ orgSlug: String, _ slug: String, callback: @escaping (Result<Pipeline>) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/pipelines/\(slug)") { result in
+    public func pipeline(_ orgSlug: String, _ slug: String, parameters: [String: Any]? = nil, callback: @escaping (Result<Pipeline>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/pipelines/\(slug)", parameters: parameters) { result in
             switch result {
             case .success(let value):
                 callback(Result.success(Pipeline(json: value)))
@@ -103,8 +102,8 @@ public class SwiftBuildkite {
     
     // Builds
 
-    public func builds(callback: @escaping (Result<Builds>) -> Void) -> Void {
-        request(.get, "/builds") { result in
+    public func builds(parameters: [String: Any]? = nil, callback: @escaping (Result<Builds>) -> Void) -> Void {
+        request(.get, "/builds", parameters: parameters) { result in
             switch result {
             case .success(let value):
                 callback(Result.success(Builds(json: value)))
@@ -114,8 +113,8 @@ public class SwiftBuildkite {
         }
     }
     
-    public func builds(_ orgSlug: String, callback: @escaping (Result<Builds>) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/builds") { result in
+    public func builds(_ orgSlug: String, parameters: [String: Any]? = nil, callback: @escaping (Result<Builds>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/builds", parameters: parameters) { result in
             switch result {
             case .success(let value):
                 callback(Result.success(Builds(json: value)))
@@ -125,8 +124,8 @@ public class SwiftBuildkite {
         }
     }
     
-    public func builds(_ orgSlug: String, _ pipeSlug: String, callback: @escaping (Result<Builds>) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds") { result in
+    public func builds(_ orgSlug: String, _ pipeSlug: String, parameters: [String: Any]? = nil, callback: @escaping (Result<Builds>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds", parameters: parameters) { result in
             switch result {
             case .success(let value):
                 callback(Result.success(Builds(json: value)))
@@ -136,8 +135,8 @@ public class SwiftBuildkite {
         }
     }
 
-    public func build(_ orgSlug: String, _ pipeSlug: String, _ number: Int, callback: @escaping (Result<Build>) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds/\(number)") { result in
+    public func build(_ orgSlug: String, _ pipeSlug: String, _ number: Int, parameters: [String: Any]? = nil, callback: @escaping (Result<Build>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds/\(number)", parameters: parameters) { result in
             switch result {
             case .success(let value):
                 callback(Result.success(Build(json: value)))
@@ -149,8 +148,8 @@ public class SwiftBuildkite {
     
     // Agents
     
-    public func agents(_ orgSlug: String, callback: @escaping (Result<Agents>) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/agents") { result in
+    public func agents(_ orgSlug: String, parameters: [String: Any]? = nil, callback: @escaping (Result<Agents>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/agents", parameters: parameters) { result in
             switch result {
             case .success(let value):
                 callback(Result.success(Agents(json: value)))
@@ -160,8 +159,8 @@ public class SwiftBuildkite {
         }
     }
     
-    public func agent(_ orgSlug: String, _ id: String, callback: @escaping (Result<Agent>) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/agents/\(id)") { result in
+    public func agent(_ orgSlug: String, _ id: String, parameters: [String: Any]? = nil, callback: @escaping (Result<Agent>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/agents/\(id)", parameters: parameters) { result in
             switch result {
             case .success(let value):
                 callback(Result.success(Agent(json: value)))
@@ -173,8 +172,8 @@ public class SwiftBuildkite {
     
     // Artifacts
 
-    public func artifacts(_ orgSlug: String, _ pipeSlug: String, _ number: Int, callback: @escaping (Result<Artifacts>) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds/\(number)/artifacts") { result in
+    public func artifacts(_ orgSlug: String, _ pipeSlug: String, _ number: Int, parameters: [String: Any]? = nil, callback: @escaping (Result<Artifacts>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds/\(number)/artifacts", parameters: parameters) { result in
             switch result {
             case .success(let value):
                 callback(Result.success(Artifacts(json: value)))
@@ -184,8 +183,8 @@ public class SwiftBuildkite {
         }
     }
     
-    public func artifacts(_ orgSlug: String, _ pipeSlug: String, _ number: Int, _ id: String, callback: @escaping (Result<Artifacts>) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds/\(number)/jobs/\(id)/artifacts") { result in
+    public func artifacts(_ orgSlug: String, _ pipeSlug: String, _ number: Int, _ id: String, parameters: [String: Any]? = nil, callback: @escaping (Result<Artifacts>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds/\(number)/jobs/\(id)/artifacts", parameters: parameters) { result in
             switch result {
             case .success(let value):
                 callback(Result.success(Artifacts(json: value)))
@@ -195,8 +194,8 @@ public class SwiftBuildkite {
         }
     }
 
-    public func artifact(_ orgSlug: String, _ pipeSlug: String, _ number: Int, _ id: String, callback: @escaping (Result<Artifact>) -> Void) -> Void {
-        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds/\(number)/artifacts/\(id)") { result in
+    public func artifact(_ orgSlug: String, _ pipeSlug: String, _ number: Int, _ id: String, parameters: [String: Any]? = nil, callback: @escaping (Result<Artifact>) -> Void) -> Void {
+        request(.get, "/organizations/\(orgSlug)/pipelines/\(pipeSlug)/builds/\(number)/artifacts/\(id)", parameters: parameters) { result in
             switch result {
             case .success(let value):
                 callback(Result.success(Artifact(json: value)))
